79aed4b3dbb9b803e952c38b333950fb4c09cf72
aditiraipure <raipureaditi22@gmail.com>
2026-03-31 20:23:00 +0530

    Final_Project_

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
 import ChatBox from "./pages/ChatBox";
-import Connection from "./pages/connection";
+import Connection from "./pages/Connection";
 import Discover from "./pages/Discover";
 import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
