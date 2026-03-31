d1fc01b54abfdcc79423bf54c4b5636b54db7302
aditiraipure <raipureaditi22@gmail.com>
2026-03-31 20:49:50 +0530

    temp rename

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Login from "./pages/Login";
 import Feed from "./pages/Feed";
 import Messages from "./pages/Messages";
 import ChatBox from "./pages/ChatBox";
-import Connection from "./pages/Connection";
+import Connection from "./pages/connection.jsx";
 import Discover from "./pages/Discover";
 import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
